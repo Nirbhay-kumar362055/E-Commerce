fbd37222dad714915700aa7bc71f9b242230794c
Nirbhay-kumar <nirbhaykumar362055@gmail.com>
2025-07-02 17:42:07 +0530

    added place order feature

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -2,6 +2,7 @@ import { createContext  } from "react";
 import { products } from "../assets/assets";
 import { useState , useEffect } from "react";
 import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 export const ShopContext = createContext();
 
@@ -11,6 +12,7 @@ const ShopContextProvider = (props)=>{
     const [search , setSearch] = useState('');
     const [showSearch , setShowSearch] = useState(false);
     const [cartItems , setCartItems] = useState({});
+    const navigate = useNavigate();
 
     const addToCart = async(itemId , size) => {
         if(!size){
@@ -58,7 +60,7 @@ const ShopContextProvider = (props)=>{
 
     }
 
-    const getCartAmount = async ()=> {
+    const getCartAmount =  ()=> {
         let totalAmount = 0;
         for(const items in cartItems){
             let itemInfo = products.find((item => item._id === items));
@@ -80,7 +82,7 @@ const ShopContextProvider = (props)=>{
         currency,
         delivery_fee,
         search,setSearch, showSearch , setShowSearch ,
-        cartItems , addToCart , getCartCount,updateQuantity
+        cartItems , addToCart , getCartCount,updateQuantity , getCartAmount , navigate
 
     }
 
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -3,9 +3,11 @@ import { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../context/ShopContext";
 import Title from "../components/Title";
 import { assets } from "../assets/assets.js";
+import CartTotal from "../components/CartTotal.jsx";
+
 
 const Cart = () => {
-  const { products, currency, cartItems, updateQuantity } =
+  const { products, currency, cartItems, updateQuantity  , navigate} =
     useContext(ShopContext);
   const [cartData, setCartData] = useState([]);
   useEffect(() => {
@@ -83,6 +85,15 @@ const Cart = () => {
             </div>
           );
         })}
+      </div>
+      <div className="flex justify-end my-20">
+        <div className="w-ful sm:w-[450px]">
+          <CartTotal />
+          <div className="w-full text-end">
+            <button onClick={() => navigate('/placeOrder')} className="bg-black text-white text-sm my-8 px-8 py-3">PROCEED TO CHECKOUT</button>
+
+          </div>
+        </div>
       </div>
     </div>
   );
